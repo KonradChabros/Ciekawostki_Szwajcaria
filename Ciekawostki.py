2ea9b0a8c4a9934b49728d58e4eb1b728910a34e
KonradChabros <144693219+KonradChabros@users.noreply.github.com>
2023-09-11 15:52:07 +0200

    Add files via upload

---
+++ Ciekawostki.py
@@ -0,0 +1,38 @@
+import random
+
+# List of funny and interesting facts
+facts = [
+    "Około 8 milionów ludzi mieszka w Szwajcarii. Jest to jeden z najmniejszych krajów w Europie.",
+    "Szwajcaria formalnie nie posiada stolicy. Jej funkcję de facto pełni jednak Berno. Największym miastem jest Zurych.",
+    "Najwyższą górą Szwajcarii jest Dufourspitze (Monte Rosa), ale najpopularniejszą jest Matterhorn (4 478 metrów).",
+    "W Szwajcarii znajdują się 1484 jeziora, z których największym jest jezioro Genewskie (Lac Leman), choć jego południowy brzeg znajduje się we Francji.",
+    "Szwajcaria prawdopodobnie jest największym producentem czekolady na świecie.",
+    "Piłka nożna i hokej to najpopularniejsze sporty w Szwajcarii.",
+    "Jednym z najbardziej znanych zabytków Szwajcarii jest Genewska Jet d’Eau – fontanna wody. Przeciętnie dzień strzela około 500 litrami wody na sekundę z prędkością 200 km/godz.",
+    "Ma ona 4 języki urzędowe: niemiecki, francuski, włoski i rzymski. Nie oznacza to, że każdy Szwajcar mówi więcej niż jednym językiem, ale większość to robi.",
+    "Jungfraujoch jest najwyższym dworcem kolejowym w Europie o wysokości 3453 metrów.",
+    "Jeden szwajcarski jubiler wykonał pierścień w całości z diamentu, który sprzedał za 68 milionów dolarów.",
+    "Cudzoziemcy stanowią około 23% ludności szwajcarskiej.",
+    "W 2007 roku Szwajcaria przypadkowo zaatakowała sąsiadujący z nią Liechtenstein.",
+    "W przypadku wojny nuklearnej Szwajcarzy mają bunkry, które mogą pomieścić całą ludność.",
+    "W Genewie w Szwajcarii znajduje się również druga co do wielkości z czterech głównych siedzib biurowych Organizacji Narodów Zjednoczonych.",
+    "Połowa luksusowych zegarków produkowanych na świecie pochodzi ze Szwajcarii. Takie marki jak Tissot, TAG Heuer, Rolex, Patek Philippe i Longines są szwajcarskie.",
+    "Szwajcaria dostarcza swoim obywatelom prawie tylko przyjazną dla środowiska energię elektryczną. 39% energii elektrycznej pochodzi z elektrowni jądrowych, a 56% z elektrowni wodnych.",
+    "Eutanazja w Szwajcarii jest legalna. Jest również legalna w Chinach, Belgii, Kanadzie i kilku innych krajach. Istnieją jednak różne przepisy dotyczące okoliczności, w których jest to legalne.",
+    "Wielki Zderzacz Hadronów, LHC – największy na świecie akcelerator cząstek, znajdujący się w Europejskim Ośrodku Badań Jądrowych CERN w pobliżu Genewy.",
+    "Według Indeksu Jakości Życia Economist Intelligence Unit, Szwajcaria jest najlepszym miejscem na narodziny. Uwzględnia się przy tym zatrudnienie, poziom przestępczości, jakość życia, opiekę zdrowotną, szczęście itp.",
+
+
+
+
+
+
+
+]
+
+# Select a random fact
+random_fact = random.choice(facts)
+
+# Print the random fact
+print("Ciekawostka o Szwajcarii:")
+print(random_fact)
